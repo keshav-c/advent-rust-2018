1698ed5d5b0c929788953b2411b6060ecf803c27
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2024-07-06 14:45:41 +0530

    Add day1 tests

--- day1/src/frequency.rs
+++ day1/src/frequency.rs
@@ -2,49 +2,42 @@ use std::collections::HashSet;
 
 pub struct Freq {
     shift_list: Vec<i32>,
-    change_set: HashSet<i32>,
-    current_delta: i32,
     repeat: Option<i32>,
-    current_index: usize,
 }
 
 impl Freq {
     pub fn new(shift_list: Vec<i32>) -> Self {
-        let mut change_set: HashSet<i32> = HashSet::new();
-        let current_delta = 0;
-        change_set.insert(current_delta);
         Freq {
             shift_list,
-            change_set,
-            current_delta,
             repeat: None,
-            current_index: 0usize,
         }
-    }
-
-    fn inc_index(&mut self) {
-        let len = self.shift_list.len();
-        self.current_index = (self.current_index + 1) % len;
     }
 }
 
 pub struct FreqIterator<'a> {
     freq: &'a mut Freq,
+    current_index: usize,
+    current_delta: i32,
+    change_set: HashSet<i32>,
+}
+
+impl FreqIterator<'_> {
+    fn inc_index(&mut self) {
+        let len = self.freq.shift_list.len();
+        self.current_index = (self.current_index + 1) % len;
+    }
 }
 
 impl<'a> Iterator for FreqIterator<'a> {
     type Item = i32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let current_item = self.freq.shift_list[self.freq.current_index];
-        let new_change = self.freq.current_delta + current_item;
-        println!(
-            "{} + {} = {}",
-            self.freq.current_delta, current_item, new_change
-        );
-        if self.freq.change_set.insert(new_change) {
-            self.freq.current_delta = new_change;
-            self.freq.inc_index();
+        let current_item = self.freq.shift_list[self.current_index];
+        let new_change = self.current_delta + current_item;
+        // println!("{} + {} = {}", self.current_delta, current_item, new_change);
+        if self.change_set.insert(new_change) {
+            self.current_delta = new_change;
+            self.inc_index();
             Some(current_item)
         } else {
             self.freq.repeat = Some(new_change);
@@ -58,7 +51,15 @@ impl<'a> IntoIterator for &'a mut Freq {
     type IntoIter = FreqIterator<'a>;
 
     fn into_iter(self) -> Self::IntoIter {
-        FreqIterator { freq: self }
+        let current_delta = 0;
+        let mut change_set: HashSet<i32> = HashSet::new();
+        change_set.insert(current_delta);
+        FreqIterator {
+            freq: self,
+            current_index: 0,
+            current_delta,
+            change_set,
+        }
     }
 }
 
@@ -72,5 +73,37 @@ mod tests {
         let mut freq = Freq::new(v);
         for _ in &mut freq {}
         assert_eq!(freq.repeat, Some(2))
+    }
+
+    #[test]
+    fn test2() {
+        let v = vec![1, -1];
+        let mut freq = Freq::new(v);
+        for _ in &mut freq {}
+        assert_eq!(freq.repeat, Some(0))
+    }
+
+    #[test]
+    fn test3() {
+        let v = vec![3, 3, 4, -2, -4];
+        let mut freq = Freq::new(v);
+        for _ in &mut freq {}
+        assert_eq!(freq.repeat, Some(10))
+    }
+
+    #[test]
+    fn test4() {
+        let v = vec![-6, 3, 8, 5, -6];
+        let mut freq = Freq::new(v);
+        for _ in &mut freq {}
+        assert_eq!(freq.repeat, Some(5))
+    }
+
+    #[test]
+    fn test5() {
+        let v = vec![7, 7, -2, -7, -4];
+        let mut freq = Freq::new(v);
+        for _ in &mut freq {}
+        assert_eq!(freq.repeat, Some(14))
     }
 }
