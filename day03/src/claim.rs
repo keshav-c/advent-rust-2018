a703d3a2bda53a6a2ed5e8da3b17ed92b3997918
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2024-07-09 09:26:22 +0530

    Day 3: finish input parsing

--- day03/src/claim.rs
+++ day03/src/claim.rs
@@ -1,3 +1,7 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct Claim {
     id: String,
     pub coordinates: (i32, i32),
@@ -16,8 +20,64 @@ impl Claim {
     }
 }
 
-// impl TryFrom<&str> for Claim {
-//     type Error = ();
+impl TryFrom<&str> for Claim {
+    type Error = String;
 
-//     fn try_from(value: &str) -> Result<Self, Self::Error> {}
-// }
+    fn try_from(raw_claim: &str) -> Result<Self, Self::Error> {
+        static RE: Lazy<Regex> = Lazy::new(|| {
+            Regex::new(r"#(?P<id>\S+) @ (?P<x>\d+),(?P<y>\d+): (?P<w>\d+)x(?P<h>\d+)").unwrap()
+        });
+        if let Some(c) = RE.captures(raw_claim) {
+            let id = &c["id"];
+            let Ok(x) = c["x"].parse::<i32>() else {
+                return Err(String::from("x could not be parsed"));
+            };
+            let Ok(y) = c["y"].parse::<i32>() else {
+                return Err(String::from("y could not be parsed"));
+            };
+            let coordinates = (x, y);
+            let Ok(width) = c["w"].parse::<i32>() else {
+                return Err(String::from("w could not be parsed"));
+            };
+            let Ok(height) = c["h"].parse::<i32>() else {
+                return Err(String::from("h could not be parsed"));
+            };
+            Ok(Claim::new(&id, coordinates, width, height))
+        } else {
+            Err(String::from("Failed to capture groups"))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_capture1() {
+        let raw = "#1 @ 1,3: 4x4";
+        let claim: Claim = raw.try_into().unwrap();
+        assert_eq!(claim, Claim::new("1", (1, 3), 4, 4));
+    }
+
+    #[test]
+    fn test_capture2() {
+        let raw = "#2 @ 3,1: 5x4";
+        let claim: Claim = raw.try_into().unwrap();
+        assert_eq!(claim, Claim::new("2", (3, 1), 5, 4));
+    }
+
+    #[test]
+    fn test_capture3() {
+        let raw = "#3 @ 5,5: 2x3";
+        let claim: Claim = raw.try_into().unwrap();
+        assert_eq!(claim, Claim::new("3", (5, 5), 2, 3));
+    }
+
+    #[test]
+    fn test_capture4() {
+        let raw = "#3 @ 5,: 2x3";
+        let claim = Claim::try_from(raw);
+        assert_eq!(claim, Err("Failed to capture groups".into()));
+    }
+}
--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -1,17 +1,44 @@
-use std::fs;
-
-use regex::Regex;
-
 mod cell;
 mod claim;
 mod fabric;
 
-// fn read_input(path: &str) {
-//     let re = Regex::new(r"#(?P<id>\S+) @ (?P<x>\d+),(?P<y>\d+): (?P<width>\d+)x(?P<height>\d+)")
-//         .unwrap();
-//     let input = fs::read_to_string(path).unwrap();
-//     input.lines().map(|l| {
-//         let captures = re.captures(l);
-//     })
-//     // #1 @ 1,3: 4x4
-// }
+use claim::Claim;
+use std::fs;
+
+fn read_input(path: &str) -> Vec<Claim> {
+    let input = fs::read_to_string(path).unwrap();
+    input
+        .lines()
+        .map(|l| Claim::try_from(l))
+        .filter_map(|r| match r {
+            Ok(claim) => Some(claim),
+            Err(message) => {
+                eprintln!("{}", message);
+                None
+            }
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sample() {
+        let expected_input = vec![
+            Claim::new("1", (1, 3), 4, 4),
+            Claim::new("2", (3, 1), 4, 4),
+            Claim::new("3", (5, 5), 2, 2),
+        ];
+        let input = read_input("sample.txt");
+        assert_eq!(input, expected_input);
+    }
+
+    #[test]
+    fn test_bad_sample() {
+        let expected_input = vec![Claim::new("1", (1, 3), 4, 4), Claim::new("3", (5, 5), 2, 2)];
+        let input = read_input("bad_sample.txt");
+        assert_eq!(input, expected_input);
+    }
+}
