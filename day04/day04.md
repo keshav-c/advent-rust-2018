a9313dc8c35cda0575417a81c1b299472658e6fe
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2024-07-14 18:49:53 +0530

    Day 04: sort logs

--- day04/day04.md
+++ day04/day04.md
@@ -1,4 +1,11 @@
 Approach
 
 First read input
+
+1. Define a `RawLog` struct which will hold the unprocessed log lines
+2. Implement `TryFrom<&str>` for `RawLog` in order to parse the input text data and show errors if any
+3. The input is not sorted in chronological order. So I define a wrapper struct which will hold the raw logs in a sorted order.
+4. To make a vector of `RawLog`s sortable by their timestamp I need to implement `Ord` for `RawLog`
+5. As per the documentation of `std::slice::sort` the type needs to implement `Ord` trait. The instructions for how to do this are in `std::cmp::Ord` documentation.
+
 Then Sort by timestamp
--- day04/src/raw_log.rs
+++ day04/src/raw_log.rs
@@ -1,7 +1,9 @@
 use once_cell::sync::Lazy;
 use regex::Regex;
+use std::cmp::Ordering;
 use time::{macros::format_description, PrimitiveDateTime};
 
+#[derive(Eq, Debug)]
 pub struct RawLog {
     time: PrimitiveDateTime,
     message: String,
@@ -28,13 +30,48 @@ impl TryFrom<&str> for RawLog {
     }
 }
 
+impl Ord for RawLog {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.unix_timestamp().cmp(&other.unix_timestamp())
+    }
+}
+
+impl PartialOrd for RawLog {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for RawLog {
+    fn eq(&self, other: &Self) -> bool {
+        self.unix_timestamp() == other.unix_timestamp()
+    }
+}
+
+impl RawLog {
+    fn unix_timestamp(&self) -> i64 {
+        self.time.assume_utc().unix_timestamp()
+    }
+}
+
+pub struct RawLogs {
+    logs: Vec<RawLog>,
+}
+
+impl RawLogs {
+    fn new(mut logs: Vec<RawLog>) -> Self {
+        logs.sort();
+        RawLogs { logs }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use time::Month;
 
     #[test]
-    fn test_rawlog_conv_str2rawlog() {
+    fn rawlog_conv_str2rawlog() {
         let raw = "[1518-11-01 23:01] Guard #10 begins shift";
         let rl: RawLog = raw.try_into().unwrap();
         assert_eq!(rl.message, String::from("Guard #10 begins shift"));
@@ -47,15 +84,32 @@ mod tests {
 
     #[test]
     #[should_panic(expected = "Time Parsing Error")]
-    fn test_rawlog_conv_bad_timestamp() {
+    fn rawlog_conv_bad_timestamp() {
         let raw = "[1518-11-1 23:02] Guard #10 begins shift";
         let _rl: RawLog = raw.try_into().unwrap();
     }
 
     #[test]
     #[should_panic(expected = "message is empty")]
-    fn test_rawlog_conv_bad_message() {
+    fn rawlog_conv_bad_message() {
         let raw = "[1518-11-01 23:02]       ";
         let _rl: RawLog = raw.try_into().unwrap();
+    }
+
+    #[test]
+    fn rawlogs_init() {
+        let (rl1, rl2, rl3, rl4) = dummy_logs();
+        let input = vec![rl3, rl1, rl4, rl2];
+        let collection = RawLogs::new(input);
+        let (rl1, rl2, rl3, rl4) = dummy_logs();
+        assert_eq!(collection.logs, vec![rl1, rl2, rl3, rl4])
+    }
+
+    fn dummy_logs() -> (RawLog, RawLog, RawLog, RawLog) {
+        let rl1: RawLog = "[1518-11-01 00:00] message".try_into().unwrap();
+        let rl2: RawLog = "[1518-11-01 00:05] message".try_into().unwrap();
+        let rl3: RawLog = "[1518-11-01 00:25] message".try_into().unwrap();
+        let rl4: RawLog = "[1518-11-01 00:30] message".try_into().unwrap();
+        (rl1, rl2, rl3, rl4)
     }
 }
